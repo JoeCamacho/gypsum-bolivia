6ca0cd0032a2a66c54054fe4a835d434c4a93788
wari1986 <nicolaycamacho@gmail.com>
2021-03-05 15:10:29 -0400

    Hero section completed (need new png)

--- gypsum-bolivia/src/pages/index.js
+++ gypsum-bolivia/src/pages/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Layout from '../components/Layout';
 import { header } from '../styles/home.module.css';
+import Link from 'gatsby';
 
 export default function Home() {
   return (
     <Layout>
       <section className={header}>
         <div>
-          <h2>Design</h2>
-          <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer</p>
+          <img src='/banner.png' alt="home banner" style={{ maxWidth: '60%' }} />
         </div>
+        <div>
+          <h2>Gypsum </h2>
+          <h3>Construyendo Bolivia</h3>
+          <p>37 años de experiencia en el mercado ofreciendo productos de alta calidad para los hogares bolivianos.</p>
+        </div>
+        {/* <Link className={btn}>LLamanos</Link> */}
+      </section>
+      <section>
+
       </section>
     </Layout>
   )
